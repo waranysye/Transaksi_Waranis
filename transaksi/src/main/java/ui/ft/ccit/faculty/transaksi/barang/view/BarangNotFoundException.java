1444b4baec36918c6ac8bd1ebb7da2dc35a25c74
Waran <waranisayu28@gmail.com>
2025-12-28 14:19:38 +0700

    Delete transaksi/src/main/java/ui/ft/ccit/faculty/transaksi/barang directory

--- transaksi/src/main/java/ui/ft/ccit/faculty/transaksi/barang/view/BarangNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package ui.ft.ccit.faculty.transaksi.barang.view;
-
-public class BarangNotFoundException extends RuntimeException {
-
-    public BarangNotFoundException(String idBarang) {
-        super("Barang dengan id " + idBarang + " tidak ditemukan");
-    }
-}
